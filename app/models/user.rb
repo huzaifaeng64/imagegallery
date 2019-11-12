2dad269d024093586242a9bbb642c456d821af11
iMac <imac@Hussains-iMac.local>
2019-11-12 19:53:44 +0500

    commit for rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,34 +1,36 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   attr_accessor :current_password
-	after_create :assign_default_role
+  after_create :assign_default_role
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, presence: true
   has_many :attachments, dependent: :destroy
   accepts_nested_attributes_for :attachments
 
   has_many :reactions
-  
+
   def admin?
-  	has_role?(:admin)
+    has_role?(:admin)
   end
 
   def client?
-  	has_role?(:client)
+    has_role?(:client)
   end
 
   def visitor?
-  	has_role?(:visitor)
+    has_role?(:visitor)
   end
 
   def assign_default_role
-  	self.add_role(:client) if self.roles.blank?
+    add_role(:client) if roles.blank?
   end
 
-  scope :admin, -> { joins(:roles).where(roles: {name: 'admin'}) }
-  scope :client, -> { joins(:roles).where(roles: {name: 'client'}) }
-
+  scope :admin, -> { joins(:roles).where(roles: { name: 'admin' }) }
+  scope :client, -> { joins(:roles).where(roles: { name: 'client' }) }
 end
